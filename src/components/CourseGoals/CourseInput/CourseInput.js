64510dd942130d2503903e2ede886398929191a3
Faysal Regal <faysalregal@gmail.com>
2021-10-11 20:41:07 +0100

    setCSSstyleDynamically

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,19 +5,28 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setisValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0) {
+      setisValid(true); //This will reset background and border color to normal when user starts typing a goal in goal input.
+    }
     setEnteredValue(event.target.value);
+
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+        setisValid(false)
+    return; //This stops further execution of function if condition above is met.
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
