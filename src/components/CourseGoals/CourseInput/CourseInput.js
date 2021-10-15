e2dfad8b0773f6dc63cb211d1445452ad3f35e2e
f-regal <faysalregal@gmail.com>
2021-10-15 17:37:05 +0100

    AddedCSSModules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,41 +1,53 @@
+//STYLED COMPONENTS METHOD
+// import React, { useState } from 'react';
+// import styled from 'styled-components'
+// import Button from '../../UI/Button/Button';
+// import './CourseInput.css';
+
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   color: ${props => props.invalid ? 'red' : 'black'};
+// }
+
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid #ccc;
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+//   border-color: ${props => props.invalid ? 'red' : '#ccc'};
+//   background-color: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
+// }
+
+// & input:focus {
+//   outline: none;
+//   background: #fad0ec;
+//   border-color: #8b005d;
+// }
+
+// &.invalid label {
+//   color: red;
+// }
+// `;
+
+
+
+
+
+
+//CSS MODULES METHOD
+
 import React, { useState } from 'react';
-import styled from 'styled-components'
-
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
-
-
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  color: ${props => props.invalid ? 'red' : 'black'};
-}
-
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid #ccc;
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-  border-color: ${props => props.invalid ? 'red' : '#ccc'};
-  background-color: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
-}
-
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
-
-&.invalid label {
-  color: red;
-}
-`;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -58,17 +70,14 @@ const CourseInput = props => {
     props.onAddGoal(enteredValue);
   };
 
-  // <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
-  //       <label>Course Goal</label>
-  //       <input type="text" onChange={goalInputChangeHandler} />
-  // </div>
+
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler}/>
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
